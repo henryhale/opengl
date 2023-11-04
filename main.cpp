beaae79c969151b05c553e9c201b96334e132b8e
Henry Hale <henryhale@users.noreply.github.com>
2023-11-04 17:26:34 -0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,37 @@ void display(void);
 
 int main(int argc, char* argv[])
 {
+    // initializes GLUT
     glutInit(&argc, argv);
+
+    // set display mode (single buffer with RGB colors 
+    // since there are no animations
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowPosition(200,300);
-    glutInitWindowSize(400,400);
+
+    // set window initial position
+    glutInitWindowPosition(200, 300);
+    
+    // set window initial size
+    glutInitWindowSize(400, 400);
+
+    // create the window with a title e.g "Hello OpenGL"
     glutCreateWindow("Hello OpenGL");
 
+    // register the display function, triggered whenever a new 
+    // frame is required
     glutDisplayFunc(display);
 
+    // start the event loop: the system will listen for events 
+    // and trigger their respective callback functions
     glutMainLoop();
 
     return (0);
 }
 
+// function that draws on the canvas (window in this case)
 void display(void)
 {
-
+    // set the clear color (more like the color of the rubber)
     glClearColor(0, 0, 0, 1);
 
     // clear the display using the clear color         
